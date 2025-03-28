1e85073c3e56f1dc5ddb3141af0325ac5a1cfbb6
Conrad Lineker-Jones <33847899+Chattox@users.noreply.github.com>
2025-03-28 11:10:02 +0000

    feat: colour scheme switcher (#3)
    * feat: set initial colour scheme to system colour scheme
    
    * feat: colour scheme toggle switch
    
    * feat: colour scheme switcher styling

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,14 +1,18 @@
-import { Box, Stack, Title } from "@mantine/core";
+import { Box, Group, Stack, Title } from "@mantine/core";
 import { LatestReadingDisplay } from "../LatestReadingDisplay";
 import { HistoryDisplay } from "../HistoryDisplay";
+import { ColorSchemeSwitcher } from "../ColorSchemeSwitcher/ColorSchemeSwitcher";
 
 export const Dashboard = () => {
   return (
     <Box p="xs">
       <Stack>
-        <Title order={2} mb="md">
-          Sunwise Dashboard
-        </Title>
+        <Group w="100%" justify="space-between">
+          <Title order={2} mb="md">
+            Sunwise Dashboard
+          </Title>
+          <ColorSchemeSwitcher />
+        </Group>
         <Title order={4}>Current conditions</Title>
         <LatestReadingDisplay station="sunwise" />
         <HistoryDisplay station="sunwise" />
